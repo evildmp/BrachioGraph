c72d8e4c6d0d956360f597f0b693d34f8eed97ae
Daniele Procida <daniele@vurt.org>
2019-12-07 18:09:49 +0000

    Continued work on turtle graphics

---
+++ bgt.py
@@ -0,0 +1,28 @@
+from turtle_draw import BrachioGraphTurtle
+
+# This is an example BrachioGraphTurtle definition.
+
+import time
+
+bgt = BrachioGraphTurtle(
+    inner_arm=8,          # the length of the inner arm (blue)
+    outer_arm=8,          # the length of the outer arm (red)
+
+    shoulder_centre_angle=-45,  # the starting angle of the inner arm, relative to straight ahead
+    shoulder_sweep=120,     # the arc covered by the shoulder motor
+
+    elbow_centre_angle=90,  # the centre of the outer arm relative to the inner arm
+    elbow_sweep=120,        # the arc covered by the elbow motor
+
+    window_size=800,        # width and height of the turtle canvas
+    speed=0
+)
+
+
+if __name__ == '__main__':
+
+    bgt.draw_grid()
+    bgt.draw_arcs()
+    bgt.draw_arms()
+    bgt.draw_outline()
+    bgt.screen.exitonclick()
