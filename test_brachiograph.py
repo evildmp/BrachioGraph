15a85e55e1631f3d9bf5370d1fcf47098364649b
Daniele Procida <daniele@vurt.org>
2022-02-16 23:07:53 +0000

    Made tests work again

--- test_brachiograph.py
+++ test_brachiograph.py
@@ -14,7 +14,7 @@ virtual_bg = BrachioGraph(
 # ----------------- drawing methods -----------------
 
 def test_plot_from_file():
-    virtual_bg.plot_file("test-patterns/test-pattern.json")
+    virtual_bg.plot_file("test-patterns/accuracy.json")
 
 
 # ----------------- test pattern methods -----------------
@@ -51,14 +51,3 @@ def test_maths_errors():
     )
     with pytest.raises(Exception):
         plotter.xy_to_angles(-10.2, 13.85)
-
-
-# ----------------- end-to-end tests -----------------
-
-def test_linedraw_to_plot():
-    linedraw.image_to_json(
-        "../test-patterns/test_pattern", resolution=1024,
-        draw_contours=10, repeat_contours=1,
-        draw_hatch=32, repeat_hatch=1,
-        )
-    virtual_bg.plot_file("images/test_gradient.json")
