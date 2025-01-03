d830cadd26bc89e5d75abfd7aa4a09472d139b93
Daniele Procida <daniele@vurt.org>
2025-01-03 20:14:17 +0000

    Improved lines.py

--- lines.py
+++ lines.py
@@ -1,23 +1,31 @@
 import math
 import json
-import tqdm
 
 
 class LinesMixin:
 
+    # filename, lines, bounds, angular_step, wait, resolution, draw, repeat, reverse, both, flip, rotate
+
+
     #  ----------------- plotting methods -----------------
 
-    def plot_file(self, filename="", bounds=None, angular_step=None, wait=None, resolution=None):
-        """Plots and image encoded as JSON lines in ``filename``. Passes the lines in the supplied
+    def plot_file(
+        self, 
+        filename="", 
+        bounds=None, 
+        angular_step=None, 
+        wait=None, 
+        resolution=None,
+        flip=False, 
+        rotate=False,
+        ):
+        """Plots an image encoded as JSON lines in ``filename``. Passes the lines in the supplied
         JSON file to ``plot_lines()``.
         """
 
-        bounds = bounds or self.bounds
-
         with open(filename, "r") as line_file:
             lines = json.load(line_file)
-
-        self.plot_lines(lines, bounds, angular_step, wait, resolution, flip=True)
+        self.plot_lines(lines, bounds, angular_step, wait, resolution, flip, rotate) 
 
     def plot_lines(
         self,
@@ -31,11 +39,9 @@ class LinesMixin:
     ):
         """Passes each segment of each line in lines to ``draw_line()``"""
 
-        bounds = bounds or self.bounds
+        lines = self.process_lines(lines, bounds, flip, rotate)
 
-        lines = self.rotate_and_scale_lines(lines=lines, bounds=bounds, flip=True)
-
-        for line in tqdm.tqdm(lines, desc="Lines", leave=False):
+        for line in lines:
             x, y = line[0]
 
             # only if we are not within 1mm of the start of the line, lift pen and go there
@@ -47,7 +53,7 @@ class LinesMixin:
                 self.xy(x, y, angular_step, wait, resolution, draw=True)
 
         self.park()
-
+    
     #  ----------------- x/y drawing methods -----------------
 
     def draw_line(
@@ -59,9 +65,7 @@ class LinesMixin:
         end_x, end_y = end
 
         self.xy(start_x, start_y, angular_step, wait, resolution)
-
         self.xy(end_x, end_y, angular_step, wait, resolution, draw=True)
-
         if both:
             self.xy(start_x, start_y, angular_step, wait, resolution, draw=True)
 
@@ -70,7 +74,6 @@ class LinesMixin:
         means that the pen will not be moved in that dimension.
         """
 
-        wait = wait if wait is not None else self.wait
         resolution = resolution or self.resolution
 
         x = x if x is not None else self.x
@@ -86,11 +89,6 @@ class LinesMixin:
 
             no_of_steps = round(length / resolution) or 1
 
-            if no_of_steps < 100:
-                disable_tqdm = True
-            else:
-                disable_tqdm = False
-
             (length_of_step_x, length_of_step_y) = (x_length / no_of_steps, y_length / no_of_steps)
 
             for step in range(no_of_steps):
@@ -104,45 +102,40 @@ class LinesMixin:
         else:
             self.move_angles(angle_1, angle_2, angular_step, wait, draw)
             
+
     # ----------------- line-processing methods -----------------
 
-    def rotate_and_scale_lines(self, lines=[], rotate=False, flip=False, bounds=None):
+    def process_lines(self, lines=[], bounds=None, flip=False, rotate=False):
         """Rotates and scales the lines so that they best fit the available drawing ``bounds``."""
-        (
-            rotate,
-            x_mid_point,
-            y_mid_point,
-            box_x_mid_point,
-            box_y_mid_point,
-            divider,
-        ) = self.analyse_lines(lines, rotate, bounds)
+
+        # get all the x and y values from the lines
+        x_values = y_values = set()
+        for line in lines:
+            xs, ys = zip(*line)
+            x_values.update(xs)
+            y_values.update(ys)
+
+        source_bounds = (min(x_values), min(y_values), max(x_values), max(y_values))
+        target_bounds = bounds or self.bounds 
+
+        rotate, x_mid, y_mid, box_x_mid, box_y_mid, divider = self.map_source_to_target(
+            source_bounds, target_bounds, rotate
+            )
 
         for line in lines:
-
             for point in line:
-                if rotate:
-                    point[0], point[1] = point[1], point[0]
-
-                x = point[0]
-                x = x - x_mid_point  # shift x values so that they have zero as their mid-point
-                x = x / divider  # scale x values to fit in our box width
-
-                if flip ^ rotate:  # flip before moving back into drawing pane
-                    x = -x
-
-                # shift x values so that they have the box x midpoint as their endpoint
-                x = x + box_x_mid_point
-
-                y = point[1]
-                y = y - y_mid_point
-                y = y / divider
-                y = y + box_y_mid_point
-
-                point[0], point[1] = x, y
-
+                point[0], point[1] = self.translate_point(
+                    point, 
+                    x_mid, 
+                    y_mid, 
+                    box_x_mid, 
+                    box_y_mid, 
+                    divider, 
+                    flip, 
+                    rotate)
         return lines
 
-    def analyse_lines(self, lines=[], rotate=False, bounds=None):
+    def map_source_to_target(self, source_bounds, target_bounds, rotate=False):
         """
         Analyses the co-ordinates in ``lines``, and returns:
 
@@ -174,38 +167,17 @@ class LinesMixin:
             ]
         """
 
-        bounds = bounds or self.bounds
+        # Find the min/max x and y values amongst all the points, and their ranges and mid-points
 
-        # First, we create a pair of empty sets for all the x and y values in all of the lines of
-        # the plot data.
+        source_x_range = abs(source_bounds[2] - source_bounds[0]) 
+        source_y_range = abs(source_bounds[3] - source_bounds[1])
+        source_x_mid = (source_bounds[0] + source_bounds[2]) / 2 
+        source_y_mid = (source_bounds[1] + source_bounds[3]) / 2
 
-        x_values_in_lines = set()
-        y_values_in_lines = set()
-
-        # Loop over each line and all the points in each line, to get sets of all the x and y
-        # values:
-
-        for line in lines:
-
-            x_values_in_line, y_values_in_line = zip(*line)
-
-            x_values_in_lines.update(x_values_in_line)
-            y_values_in_lines.update(y_values_in_line)
-
-        # Identify the minimum and maximum values.
-
-        min_x, max_x = min(x_values_in_lines), max(x_values_in_lines)
-        min_y, max_y = min(y_values_in_lines), max(y_values_in_lines)
-
-        # Identify the range they span.
-
-        x_range, y_range = max_x - min_x, max_y - min_y
-        box_x_range, box_y_range = bounds[2] - bounds[0], bounds[3] - bounds[1]
-
-        # And their mid-points.
-
-        x_mid_point, y_mid_point = (max_x + min_x) / 2, (max_y + min_y) / 2
-        box_x_mid_point, box_y_mid_point = (bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2
+        target_x_range = abs(target_bounds[2] - target_bounds[0]) 
+        target_y_range = abs(target_bounds[3] - target_bounds[1])
+        target_x_mid = (target_bounds[0] + target_bounds[2]) / 2 
+        target_y_mid = (target_bounds[1] + target_bounds[3]) / 2
 
         # Get a 'divider' value for each range - the value by which we must divide all x and y so
         # that they will fit safely inside the bounds.
@@ -213,17 +185,42 @@ class LinesMixin:
         # If both image and box are in portrait orientation, or both in landscape, we don't need to
         # rotate the plot.
 
-        if (x_range >= y_range and box_x_range >= box_y_range) or (
-            x_range <= y_range and box_x_range <= box_y_range
-        ):
+        if (source_x_range / source_y_range - 1) * (target_x_range / target_y_range - 1) >= 0:
 
-            divider = max((x_range / box_x_range), (y_range / box_y_range))
+            divider = 10 ** max(
+                abs(math.log10(source_x_range / target_x_range)), 
+                abs(math.log10(source_y_range / target_y_range))
+            )
             rotate = False
+            divider = 1 / divider
 
         else:
 
-            divider = max((x_range / box_y_range), (y_range / box_x_range))
+            divider = 10 ** max(
+                abs(math.log10(source_x_range / target_y_range)), 
+                abs(math.log10(source_y_range / target_x_range))
+            )
+            divider = 1 / divider
             rotate = True
-            x_mid_point, y_mid_point = y_mid_point, x_mid_point
+            source_x_mid, source_y_mid = source_y_mid, source_x_mid
 
-        return (rotate, x_mid_point, y_mid_point, box_x_mid_point, box_y_mid_point, divider)
+        return (rotate, source_x_mid, source_y_mid, target_x_mid, target_y_mid, divider)
+
+
+    def translate_point(self, point, source_x_mid, source_y_mid, target_x_mid, target_y_mid, divider, flip, rotate):
+        if not rotate:
+            x, y = point
+        else:
+            y, x = point
+
+        x = x - source_x_mid  # shift x values so that they have zero as their mid-point
+        x = x / divider  # scale x values to fit in our box width
+
+        if flip ^ rotate:  # flip before moving back into drawing pane
+            x = -x
+        # shift x values so that they have the box x midpoint as their endpoint
+        x = x + target_x_mid
+
+        point = (x, target_y_mid + (y - source_y_mid) / divider)
+        return point
+    
